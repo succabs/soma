266703ce9583397246532477c2e7e717727b7b4d
Arttu <artnikkila@gmail.com>
2023-02-27 22:43:02 +0200

    Modified the user page, added placeholder profile picture

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -6,7 +6,6 @@ export default function Root() {
       <>  
       
         <Navbar />
-        
         <div id="outlet">
         <Outlet />
         </div>
--- src/routes/user.jsx
+++ src/routes/user.jsx
@@ -1,20 +1,30 @@
 import "../index.css"
+import profilepic from '../assets/profilepic.jpeg'; 
 export default function User() {
   
     return (
 
           <div className="userPage">
             <div className="userMainArea">
-            <h1>User page</h1>
-            <p>This is a sample user profile page.</p>
-            <p>This is a sample user profile page.</p>
-            <p>This is a sample user profile page.</p>
-            <p>This is a sample user profile page.</p>
+            
+                <div className="userGreetings">
+                <img src={profilepic} alt="Profile picture" />
+                    <h1>Nick Arty</h1>
 
+            <h5>Welcome to my profile page! This page is super good! Lorem ipsum dolor sit amet! Filler text</h5>
             </div>
-            <div className="userSideBar">
-            <p>Edit your page</p>
 
+            <div className="post">
+            <p>This is a post on my page.</p>
+            </div>
+            <div className="post">
+            <p>This is a post on my page.</p>
+            </div>            <div className="post">
+            <p>This is a post on my page.</p>
+            </div>
+            <div className="post">
+            <p>This is a post on my page.</p>
+            </div>
             </div>
             </div>
     );
