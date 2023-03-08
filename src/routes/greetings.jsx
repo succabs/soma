ac3d20731c0080d134e8cd1f9825ae6f35b9cfb7
Arttu <artnikkila@gmail.com>
2023-03-08 21:47:05 +0200

    Modified css

--- src/routes/greetings.jsx
+++ src/routes/greetings.jsx
@@ -1,13 +1,12 @@
 export default function Home() {
     return (
 
-          <div className="post">
+          <div className="frontPageGreetings">
             <p>Welcome to ReadPostShare, a social media application that pays homage to the early days of social networking.
             With a focus on authentic connections and shared interests, we aim to bring back the joy and simplicity of 
             social media before it became inundated with ads and algorithms. Come rediscover the roots of social media with us.
             </p>
-            <p>Take a quick tour to see what it's all about!</p>
-            <p>Join now and rediscover the joy in social media!</p>
+            <p>Join now!</p>
           </div>
     );
   }
