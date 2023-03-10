f7689beaa3c5543fa703a44d68824b67b6f624f6
Arttu <artnikkila@gmail.com>
2023-03-10 23:52:16 +0200

    Corrected a bug in user profiles

--- src/Components/Posts.jsx
+++ src/Components/Posts.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Post from "../Components/Post";
 
 export default function Posts({ userId }) {
@@ -14,25 +14,40 @@ export default function Posts({ userId }) {
     return parsedMessages;
   });
 
+  useEffect(() => {
+    const storedMessages = localStorage.getItem("messages");
+    let parsedMessages = storedMessages ? JSON.parse(storedMessages) : [];
+    if (userId) {
+      // Filter messages based on the user id if it's passed
+      parsedMessages = parsedMessages.filter(
+        (message) => message.id === parseInt(userId)
+      );
+    }
+    setMessages(parsedMessages);
+  }, [userId]);
+
   if (messages.length > 0) {
     messages.reverse();
   }
+
   const handleDelete = (messageId) => {
-    const updatedMessages = messages.filter((message) => message.messageId !== messageId);
+    const updatedMessages = messages.filter(
+      (message) => message.messageId !== messageId
+    );
     setMessages(updatedMessages);
-    
+
     // Get all messages from localStorage
     const storedMessages = localStorage.getItem("messages");
     const allMessages = storedMessages ? JSON.parse(storedMessages) : [];
-  
+
     // Remove the specific message with the given messageId
-    const updatedAllMessages = allMessages.filter((message) => message.messageId !== messageId);
-    
+    const updatedAllMessages = allMessages.filter(
+      (message) => message.messageId !== messageId
+    );
+
     // Update localStorage with the updatedAllMessages array
     localStorage.setItem("messages", JSON.stringify(updatedAllMessages));
   };
-  
-  
 
   return (
     <div className="posts">
